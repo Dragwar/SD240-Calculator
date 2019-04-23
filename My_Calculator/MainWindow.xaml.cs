310c174227ca9095f10d89b6d9f6197377ca69b9
Everett Grassler <dragwar9@gmail.com>
2019-04-23 01:24:24 -0500

    Fixed min-width

--- My_Calculator/MainWindow.xaml.cs
+++ My_Calculator/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using My_Calculator.Helpers.Enums;
 
 namespace My_Calculator
 {
@@ -9,17 +13,60 @@ namespace My_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<decimal> Numbers { get; set; } = new List<decimal>();
+        private const string ButtonString = "Button";
+        public List<decimal> Numbers { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Numbers = new List<decimal>();
+        }
+        private bool CompareName<TEnum>(string name, TEnum enumToCompare) where TEnum : Enum => name == (enumToCompare.ToString() + ButtonString);
+        private void InputControlClearAll()
+        {
+            TopOutputText.Text = "";
+            MainOutputText.Text = "";
+            Numbers.Clear();
+        }
+        private void InputControlClearEntry() => MessageBox
+            .Show("Clear Entry is not implemented yet", $"ClearEntry{ButtonString}", MessageBoxButton.OK, MessageBoxImage.Warning);
 
+        private void InputControlBackSpace()
+        {
+            if (Numbers.Count >= 1)
+            {
+                Numbers.RemoveAt(Numbers.Count - 1);
+                MainOutputText.Text = string.Join("", Numbers);
+            }
         }
 
         private void InputControlButton_Click(object sender, RoutedEventArgs e)
         {
+            if (sender is Button btn)
+            {
+                string btnName = btn.Name as string;
+                bool validInputControl = false;
 
+                if (!string.IsNullOrWhiteSpace(btnName))
+                {
+                    validInputControl = Enum.GetValues(typeof(InputControlEnum))
+                        .Cast<int>()
+                        .Any(@enum => CompareName(btnName, (InputControlEnum)@enum));
+                }
+
+                if (validInputControl && Enum.Parse(typeof(InputControlEnum), btnName.Replace(ButtonString, "")) is InputControlEnum useEnum)
+                {
+                    switch (useEnum)
+                    {
+                        case InputControlEnum.BackSpace: InputControlBackSpace(); break;
+                        case InputControlEnum.ClearEntry: InputControlClearEntry(); break;
+                        case InputControlEnum.ClearAll: InputControlClearAll(); break;
+
+                        default: throw new Exception("this shouldn't happen");
+                    }
+                }
+            }
         }
+
 
         private void MathFunctionButton_Click(object sender, RoutedEventArgs e)
         {
@@ -39,5 +86,11 @@ namespace My_Calculator
                 MainOutputText.Text = string.Join("", Numbers);
             }
         }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e) => MessageBox
+            .Show("This History Menu is not implemented yet", $"{(sender as Button).Name}", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+        private void HamburgerMenuButton_Click(object sender, RoutedEventArgs e) => MessageBox
+            .Show("This Hamburger Menu is not implemented yet", $"{(sender as Button).Name}", MessageBoxButton.OK, MessageBoxImage.Warning);
     }
 }
