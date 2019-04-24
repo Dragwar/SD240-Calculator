606783f5f6101e490d9928002d828c9b7d05662c
Everett Grassler <dragwar9@gmail.com>
2019-04-23 22:19:22 -0500

    Added keyboard listener

--- My_Calculator/MainWindow.xaml.cs
+++ My_Calculator/MainWindow.xaml.cs
@@ -20,6 +20,11 @@ namespace My_Calculator
             InitializeComponent();
             Numbers = new List<decimal>();
         }
+        private void AddNumberToList(InputNumberEnum numberEnum)
+        {
+            Numbers.Add((int)numberEnum);
+            MainOutputText.Text = string.Join("", Numbers);
+        }
         private bool CompareName<TEnum>(string name, TEnum enumToCompare) where TEnum : Enum => name == (enumToCompare.ToString() + ButtonString);
         private void InputControlClearAll()
         {
@@ -68,15 +73,12 @@ namespace My_Calculator
         }
 
 
-        private void MathFunctionButton_Click(object sender, RoutedEventArgs e)
-        {
+        private void MathFunctionButton_Click(object sender, RoutedEventArgs e) => MessageBox
+            .Show($"This {(sender as Button)?.Name} is not implemented yet", $"{(sender as Button)?.Name}", MessageBoxButton.OK, MessageBoxImage.Warning);
 
-        }
 
-        private void OperatorButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+        private void OperatorButton_Click(object sender, RoutedEventArgs e) => MessageBox
+            .Show($"This {(sender as Button)?.Name} is not implemented yet", $"{(sender as Button)?.Name}", MessageBoxButton.OK, MessageBoxImage.Warning);
 
         private void InputNumberButton_Click(object sender, RoutedEventArgs e)
         {
@@ -92,5 +94,60 @@ namespace My_Calculator
 
         private void HamburgerMenuButton_Click(object sender, RoutedEventArgs e) => MessageBox
             .Show("This Hamburger Menu is not implemented yet", $"{(sender as Button).Name}", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Tab: break;
+
+                //case Key.Return when (int)e.Key == 6: break;
+                case Key.Enter: break;
+
+                case Key.Escape: break;
+
+                case Key.Space: break;
+
+                case Key.Back: InputControlBackSpace(); break;
+
+                case Key.Left: break;
+                case Key.Up: break;
+                case Key.Right: break;
+                case Key.Down: break;
+
+                case Key.D0: AddNumberToList(InputNumberEnum.Zero); break;
+                case Key.D1: AddNumberToList(InputNumberEnum.One); break;
+                case Key.D2: AddNumberToList(InputNumberEnum.Two); break;
+                case Key.D3: AddNumberToList(InputNumberEnum.Three); break;
+                case Key.D4: AddNumberToList(InputNumberEnum.Four); break;
+                case Key.D5: AddNumberToList(InputNumberEnum.Five); break;
+                case Key.D6: AddNumberToList(InputNumberEnum.Six); break;
+                case Key.D7: AddNumberToList(InputNumberEnum.Seven); break;
+                case Key.D8: AddNumberToList(InputNumberEnum.Eight); break;
+                case Key.D9: AddNumberToList(InputNumberEnum.Nine); break;
+
+                case Key.NumPad0: AddNumberToList(InputNumberEnum.Zero); break;
+                case Key.NumPad1: AddNumberToList(InputNumberEnum.One); break;
+                case Key.NumPad2: AddNumberToList(InputNumberEnum.Two); break;
+                case Key.NumPad3: AddNumberToList(InputNumberEnum.Three); break;
+                case Key.NumPad4: AddNumberToList(InputNumberEnum.Four); break;
+                case Key.NumPad5: AddNumberToList(InputNumberEnum.Five); break;
+                case Key.NumPad6: AddNumberToList(InputNumberEnum.Six); break;
+                case Key.NumPad7: AddNumberToList(InputNumberEnum.Seven); break;
+                case Key.NumPad8: AddNumberToList(InputNumberEnum.Eight); break;
+                case Key.NumPad9: AddNumberToList(InputNumberEnum.Nine); break;
+
+                //TODO: Make operator work
+                case Key.Multiply: break;
+                case Key.Add: break;
+                case Key.Separator: break;
+                case Key.Subtract: break;
+                case Key.Decimal: break;
+                case Key.Divide: break;
+
+                default: break;
+            }
+
+        }
     }
 }
