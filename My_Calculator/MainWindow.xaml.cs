0dfba6a366cdc2a8caf32e92e946456fe5800a5f
Everett Grassler <dragwar9@gmail.com>
2019-06-10 10:17:51 -0500

    Shutdown on main window closed
    is now specified on the App.xaml now

--- My_Calculator/MainWindow.xaml.cs
+++ My_Calculator/MainWindow.xaml.cs
@@ -31,17 +31,7 @@ namespace My_Calculator
             SecondaryOutput = new List<string>();
             SizeToContent = SizeToContent.WidthAndHeight;
 
-            List<Button> buttonElements = ButtonInputs.Children
-                .OfType<Grid>()
-                .ToList()?[0]
-                .Children.OfType<Border>()
-                .ToList()
-                .Select(border => border.Child.GetType() == typeof(Button) ? border.Child as Button : null)
-                .ToList();
-
-            buttonElements.ForEach(btn => btn.Focusable = false);
-
-            Closed += (object sender, EventArgs e) => App.Current.Shutdown();
+            //Closed += (object sender, EventArgs e) => App.Current.Shutdown();
         }
         private bool CompareButtonName<TEnum>(string name, TEnum enumToCompare) where TEnum : Enum => name == (enumToCompare.ToString() + ButtonString);
         private void AddToMainOutputTextAndUpdate(string str)
